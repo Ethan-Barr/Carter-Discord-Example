afa8eab2f122213280535ab6ef4067567f04e75a
Ethan Barr <70385874+Ethan-Barr@users.noreply.github.com>
2023-04-06 22:21:50 +0100

    Update __init__.py
    Added embed messages

--- cogs/moderation/__init__.py
+++ cogs/moderation/__init__.py
@@ -5,32 +5,53 @@ class Moderation(commands.Cog):
     def __init__(self, client):
         self.client = client
 
-
     # Kick user
     @commands.command()
     @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, member: nextcord.Member, *, reason=None):
         """Kick a member from the server"""
-        await member.kick(reason=reason)
-        await ctx.send(f"{member} has been kicked from the server.")
+        try:
+            await member.kick(reason=reason)
+            await ctx.send(f"{member} has been kicked from the server.")
+        except nextcord.errors.Forbidden:
+            embed = nextcord.Embed(
+                title="Missing Permissions",
+                description="You do not have the `kick_members` permission to use this command.",
+                color=nextcord.Color.red()
+            )
+            await ctx.send(embed=embed)
 
     # Purge messages
     @commands.command()
     @commands.has_permissions(manage_messages=True)
     async def clear(self, ctx, amount: int):
         """Clears a specified amount of messages from the chat."""
-        await ctx.channel.purge(limit=amount + 1)
-        await ctx.send(f"{amount} messages have been cleared.")
+        try:
+            await ctx.channel.purge(limit=amount + 1)
+            await ctx.send(f"{amount} messages have been cleared.")
+        except nextcord.errors.Forbidden:
+            embed = nextcord.Embed(
+                title="Missing Permissions",
+                description="You do not have the `manage_messages` permission to use this command.",
+                color=nextcord.Color.red()
+            )
+            await ctx.send(embed=embed)
 
     # Ban User
     @commands.command()
     @commands.has_permissions(ban_members=True)
     async def ban(self, ctx, member: nextcord.Member, *, reason=None):
         """Bans the specified member from the server."""
-        await member.ban(reason=reason)
-        await ctx.send(f'{member.mention} has been banned.')
-
-    
+        try:
+            await member.ban(reason=reason)
+            await ctx.send(f'{member.mention} has been banned.')
+        except nextcord.errors.Forbidden:
+            embed = nextcord.Embed(
+                title="Missing Permissions",
+                description="You do not have the `ban_members` permission to use this command.",
+                color=nextcord.Color.red()
+            )
+            await ctx.send(embed=embed)
 
     # Unban User
     @commands.command()
@@ -43,9 +64,16 @@ class Moderation(commands.Cog):
             user = ban_entry.user
 
             if (user.name, user.discriminator) == (member_name, member_discriminator):
-                await ctx.guild.unban(user)
-                await ctx.send(f'Unbanned {user.name}#{user.discriminator}')
-
+                try:
+                    await ctx.guild.unban(user)
+                    await ctx.send(f'Unbanned {user.name}#{user.discriminator}')
+                except nextcord.errors.Forbidden:
+                    embed = nextcord.Embed(
+                        title="Missing Permissions",
+                        description="You do not have the `ban_members` permission to use this command.",
+                        color=nextcord.Color.red()
+                    )
+                    await ctx.send(embed=embed)
 
 
 
@@ -62,19 +90,38 @@ class Moderation(commands.Cog):
             for channel in ctx.guild.channels:
                 await channel.set_permissions(mute_role, send_messages=False)
 
-        await member.add_roles(mute_role, reason=reason)
-        await ctx.send(f'{member.mention} has been muted.')
+        try:
+            await member.add_roles(mute_role, reason=reason)
+            await ctx.send(f"{member.mention} has been muted.")
+        except nextcord.errors.Forbidden:
+            embed = nextcord.Embed(
+                title="Missing Permissions",
+                description="I do not have the `manage_roles` permission to use this command.",
+                color=nextcord.Color.red(),
+            )
+            await ctx.send(embed=embed)
 
     #Unmute User
     @commands.command()
+    @commands.has_permissions(manage_roles=True)
     async def unmute(self, ctx, member: nextcord.Member):
         """Unmutes a muted member."""
-        role = nextcord.utils.get(ctx.guild.roles, name="Muted")
-        if role not in member.roles:
-            await ctx.send(f"{member.mention} is not muted.")
+        mute_role = nextcord.utils.get(ctx.guild.roles, name="Muted")
+        if not mute_role:
+            embed = nextcord.Embed(
+                title="Mute Role Not Found",
+                description="The `Muted` role was not found. No members are currently muted.",
+                color=nextcord.Color.red()
+            )
+            await ctx.send(embed=embed)
             return
-        await member.remove_roles(role)
-        await ctx.send(f"{member.mention} has been unmuted.")
+        if mute_role not in member.roles:
+            embed = nextcord.Embed(
+                title="Member Not Muted",
+                description=f"{member.mention} is not currently muted.",
+                color=nextcord.Color.red()
+            )
+            await ctx.send(embed=embed)
 
 
 def setup(client):
